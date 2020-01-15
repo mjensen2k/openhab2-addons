aa385e901a26a88642d9b728336d3197434b98e4
Sara Damiano <sdamiano@stroudcenter.org>
2020-01-15 00:20:30 -0500

    Created binding constants
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- bundles/org.openhab.binding.bondhome/src/main/java/org/openhab/binding/bondhome/internal/BondHomeBindingConstants.java
+++ bundles/org.openhab.binding.bondhome/src/main/java/org/openhab/binding/bondhome/internal/BondHomeBindingConstants.java
@@ -12,6 +12,10 @@
  */
 package org.openhab.binding.bondhome.internal;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 
@@ -26,9 +30,35 @@ public class BondHomeBindingConstants {
 
     private static final String BINDING_ID = "bondhome";
 
-    // List of all Thing Type UIDs
-    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+    /**
+     * List of all Thing Type UIDs.
+     */
+    public static final ThingTypeUID THING_TYPE_BOND_BRIDGE = new ThingTypeUID(BINDING_ID, "bondBridge");
+    public static final ThingTypeUID THING_TYPE_BOND_FAN = new ThingTypeUID(BINDING_ID, "bondFan");
+    public static final ThingTypeUID THING_TYPE_BOND_SHADES = new ThingTypeUID(BINDING_ID, "bondShades");
+    public static final ThingTypeUID THING_TYPE_BOND_FIREPLACE = new ThingTypeUID(BINDING_ID, "bondFireplace");
+    public static final ThingTypeUID THING_TYPE_BOND_GENERIC = new ThingTypeUID(BINDING_ID, "bondGenericThing");
 
-    // List of all Channel ids
-    public static final String CHANNEL_1 = "channel1";
+    /**
+     * The supported thing types.
+     */
+    public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES = Stream.of(THING_TYPE_BOND_BRIDGE,
+            THING_TYPE_BOND_FAN, THING_TYPE_BOND_SHADES, THING_TYPE_BOND_FIREPLACE, THING_TYPE_BOND_GENERIC)
+            .collect(Collectors.toSet());
+
+    /**
+     * List of all Channel ids
+     */
+    public static final String CHANNEL_UNIT_STATE = "unitState";
+    public static final String CHANNEL_FAN_STATE = "fanState";
+    public static final String CHANNEL_FAN_SPEED = "fanSpeed";
+    public static final String CHANNEL_FAN_LIGHT_STATE = "fanLightState";
+    public static final String CHANNEL_FAN_LIGHT_BRIGHTNESS = "fanLightBrightness";
+
+    /**
+     * Configuration arguments
+     */
+    public static final String CONFIG_IP_ADDRESS = "bondIpAddress";
+    public static final String CONFIG_LOCAL_TOKEN = "localToken";
+    public static final String CONFIG_FAN_SPEEDS = "numberSpeeds";
 }
